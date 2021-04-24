6f4f89d2d790c66354bc8c681add979ad208cc3d
JoaoCLAragao <54920519+JoaoCLAragao@users.noreply.github.com>
2021-04-24 00:19:27 -0300

    Initial commit

---
+++ app/src/main/java/br/com/cotemig/a71600841_exercicio6/model/ListaProduto.kt
@@ -0,0 +1,5 @@
+package br.com.cotemig.a71600841_exercicio6.model
+
+data class ListaProduto (
+    var lista: List<Produto>
+)
---
+++ app/src/main/java/br/com/cotemig/a71600841_exercicio6/model/Produto.kt
@@ -0,0 +1,7 @@
+package br.com.cotemig.a71600841_exercicio6.model
+
+class Produto {
+    var imagem : String = ""
+    var valor : Double = 0.0
+    var nome : String = ""
+}
